b364a29138f2fb230342e228b92d2605c266ab23
msynko <marina.synko@gmail.com>
2019-01-10 14:39:36 -0500

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function removeOddSum() {
-}
+function removeOddSum(arr, k, l){
+newArray = arr.slice();
+
+for (i = 0; i < arr.length; i++) {
+  if ((arr[i][k] + arr[i][l]) % 2 !== 0) {
+    newArray.splice(arr.indexOf(newArray[i]), 1);
+        }
+    }
+    return newArray
+  }
 
 module.exports = removeOddSum;
--- main.test.js
+++ main.test.js
@@ -4,7 +4,7 @@ test('', () => {
   const result = removeOddSum([ {a: 5, b: 5},
                                 {a: 3, b: 4},
                                 {a: 2, b: 0},
-                                {a: 2, b: 1}], 'a', 'b');
+                                {a: 2, b: 1}],'a','b');
 
   expect(result).toEqual([{a: 5, b: 5}, {a: 2, b: 0}]);
 });
